04d1f720b4458b388639a87474d3f3fa8a56f8d6
Nate P <nlpruitt@gmail.com>
2021-01-28 18:00:53 -0500

    Fix type in 'package' line

--- src/main/java/com/natepruitt/backend/demo/ToDoRepository.java
+++ src/main/java/com/natepruitt/backend/demo/ToDoRepository.java
@@ -1,6 +1,6 @@
 package com.natepruitt.backend.demo.repository;
 
-import com.natepruitt.backend.demo.demo.ToDo; // import ToDo class
+import com.natepruitt.backend.demo.data.ToDo; // import ToDo class
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ToDoRepository extends MongoRepository<ToDo, String> {} // Tell MongoRepository 'entity' and type of Id attribute.
